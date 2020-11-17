a50d55ea8fd8745de9f23b6d3a432cb5844cfe84
baiyang12321 <61727433+baiyang12321@users.noreply.github.com>
2020-11-17 20:22:00 +0800

    Delete fitness_error.m

--- fitness_error.m
+++
@@ -1,7 +0,0 @@
-function error = fitness_error(X, target_train, expert1, expert2, expert3)
-  %用于遗传算法的目标函数，计算训练集总误差
-
-Overall_out=expert1.*X(1)+expert2.*X(2)+expert3.*X(3);
-error=mean(abs(Overall_out-target_train)./target_train);
-
-end
